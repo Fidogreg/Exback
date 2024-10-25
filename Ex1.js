17048deac21f8e21ff81bae396cc663c0d3a8ae7
Fidogreg <163128466+Fidogreg@users.noreply.github.com>
2024-10-24 23:03:35 -0300

    Update Ex1.js

--- Ex1.js
+++ Ex1.js
@@ -5,7 +5,7 @@ let nota3 = parseFloat(prompt("Digite a terceira nota:"));
 let media = (nota1 + nota2 + nota3) / 3;
 
 if (media >= 6) {
-  console.log("A média do aluno é", media, "e ele foi aprovado!");
+  console.log("A média do aluno é", media, "e ele está aprovado");
 } else {
-  console.log("A média do aluno é", media, "e ele foi reprovado.");
+  console.log("A média do aluno é", media, "e ele está reprovado.");
 }
